53f8dcb30fb0b4ecfc406a786d8b2a9b6d4e9cec
MuhammadAkbar <fotismenos007@mail.ru>
2022-03-22 19:18:30 +0500

    first commit

---
+++ hello-world.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 21:16:13 2022
+
+@author: MuhammadAkbar
+"""
+
+# print("Men \"Dell\" nomli")
+# print("Bilki seni \nVatan kutyapti")
+# print("Hech qachon yo'q dema")
+print("to'qqizning kvadrati", 9**2, 'ga teng')
