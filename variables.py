f2566874f80f5e4c5ef59cdb4ae66e287a2a3a98
MuhammadAkbar <fotismenos007@mail.ru>
2022-03-22 19:20:21 +0500

    first commit

---
+++ variables.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 18:51:38 2022
+
+@author: MuhammadAkbar
+"""
+
+# ism = "MuhammadAkbar"
+# yosh = 22
+# emoji = "🐯"
+# print("Mening ismim", ism, "va yoshim esa", yosh, "da", emoji)
+
+# ism = input('Ismingizni kiriting: ')
+# print("ismim: ", ism)
+
+# ism = input('Ismingizni kiriting:\n🔠')
+# print("ismim: ", ism.title())
+
+# kocha = input("Ko'cha nomi: ")
+# mahalla = input("Mahalla nomi: ")
+# tuman = input("Tuman nomi: ")
+# viloyat = input("Viloyat nomi: ")
+# print(f"Manzil: {viloyat.title()} vil. {tuman.title()} tumani, {mahalla.title()} MFY, {kocha.title()} ko'chasi")
+
+# yosh = input('Yoshingizni kiriting: ')
+# print("Siz", 2022-int(yosh), "-yilda tug'ilgansiz !")
+
+mevalar = ['olma', 'nok', 'uzum', 'anor']
+# mevalar.append('xurmo') #ohiriga obj qo'shadi
+# mevalar.insert(3, 'banan') #indexga obj qo'shadi
+# del mevalar[0] #indexdagi objni o'chirib tashlaydi
+# mevalar.remove('banan') #obj bo'yicha 1 ta birinchisini o'chirish
+# favourite = mevalar.pop(-1) #eng ohiridagi elementni sug'gurib oladi
+# mevalar.pop() #ohiridagi objni o'chirib tashladi
+# mevalar.sort() # arrni sortirovka qiladi
+# mevalar.sort(reverse=True) # arrni teskarisiga sort qiladi
+# print(sorted(mevalar, reverse=True)) # asl ro'yxatga ta'sir qilmay sort qilish
+# mevalar.reverse()
+# print(mevalar)
+# print(len(mevalar)) # uzunlik
+# sonlar = list(range(1, 11)) # [1 dan 10] gacha sonlarni yig'ib "sonlar" degan o'zgga o'zlashtiradi
+# juft_sonlar = list(range(2, 21, 2)) #[2 dan 20] gacha 2 qadamdan sonlarni yig'adi
+# print(max(juft_sonlar)) #eng katta qiymatni qaytaradi
+# print(min(juft_sonlar)) #eng kichik qiymatni qaytaradi
+# print(sum(juft_sonlar)) #barchasini yig'indisini qaytaradi
+# subMevalar = mevalar[0:2] # indexlardagi itemlarni olib qaytaradi
+# print(mevalar[:2]) # 0-dan indexgacha olib qaytaradi
+# print(mevalar[2:]) # indexdan ohirigacha olib qaytaradi
+# my_mevalarim = mevalar[:] # clone olib hotirada yangi list ochadi
+
+### Tuple = O'zgarmas ro'yxat ###
+# tomonlar = (20, 30, 55.2)
+# tomonlar = list(tomonlar) #listga aylantiradi
+# tomonlar = tuple(tomonlar) #tuplega aylantiradi
